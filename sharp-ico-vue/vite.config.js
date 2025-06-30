f5ca85534b83f96a166b9e40296ea266176ad698
Zy <1767992919@qq.com>
2025-06-30 10:56:32 +0800

    开发环境代理设置

--- sharp-ico-vue/vite.config.js
+++ sharp-ico-vue/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // 代理所有以 /api 开头的请求
+      '/api': {
+        target: 'http://localhost:5235', // 后端服务器地址
+        changeOrigin: true
+      },
+    },
+  }
 })
