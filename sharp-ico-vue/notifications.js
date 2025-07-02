d79d09d423468271c5045478045aa14b299bd4e0
Zy <1767992919@qq.com>
2025-07-02 09:20:11 +0800

    新增公告提示语

---
+++ sharp-ico-vue/notifications.js
@@ -0,0 +1,7 @@
+export const notifications = {
+    welcome: {
+        title: '公告',
+        message: '欢迎使用 sharpIcoWeb ！版本1.1.0',
+        type: 'success'
+    }
+}
--- sharp-ico-vue/src/components/SharpIco.vue
+++ sharp-ico-vue/src/components/SharpIco.vue
@@ -93,7 +93,7 @@
       <p>© {{ new Date().getFullYear() }} ICO图标转换工具 | 便捷、高效的在线图标转换服务</p>
     </footer>
   </div>
-  <a href="https://github.com/ZyPLJ" target="_blank" aria-label="View source on Github" class="github-corner">
+  <a href="https://github.com/ZyPLJ/SharpIcoWeb" target="_blank" aria-label="View source on Github" class="github-corner">
     <svg width="80" height="80" viewBox="0 0 250 250" aria-hidden="true" style="fill: rgb(21, 21, 19); color: rgb(255, 255, 255); position: absolute; top: 0; border: 0; right: 0;">
       <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" class="octo-arm" style="transform-origin: 130px 106px;"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
     </svg>
@@ -101,11 +101,12 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import {computed, onMounted, ref} from 'vue';
 import { ElMessage } from 'element-plus';
 import { Download, RefreshRight, Upload } from '@element-plus/icons-vue';
 import { uploadFile } from '../http/modules/fileUpload'
 import { ElLoading } from 'element-plus'
+import { notifications } from '../../notifications.js'
 
 // 数据定义
 const imageUrl = ref('');
@@ -114,6 +115,7 @@ const availableSizes = [16, 24, 32, 48, 64, 128, 256];
 const selectedSizes = ref([32, 48, 64]);
 const backgroundColor = ref('rgba(255, 255, 255, 0)');
 const isMultiSize = ref(false);
+import { ElNotification } from 'element-plus'
 
 // 计算属性
 const previewSizes = computed(() => {
@@ -211,6 +213,10 @@ const resetForm = () => {
   selectedSizes.value = [32, 48, 64];
   backgroundColor.value = 'rgba(255, 255, 255, 0)';
 };
+
+onMounted(() => {
+  ElNotification(notifications.welcome)
+})
 </script>
 
 <style scoped>
